a0c3671eda408b16863a7ab1a389a6416bdd6d00
BlancheTugba <blanchetugba@gmail.com>
2024-11-12 19:27:46 +0300

    Second Commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -4,5 +4,7 @@ public class main {
 public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("This is the project version 1");
+    System.out.println("This is the project version 1");
+    System.out.println("This is the project version 1");
 }
-//done with version one
+//done with version two
