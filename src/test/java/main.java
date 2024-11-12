5e58f8205f2df745739721aadf4ee301b08af417
BlancheTugba <blanchetugba@gmail.com>
2024-11-12 19:34:13 +0300

    third commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("This is the project version 1");
     System.out.println("This is the project version 1");
-    System.out.println("This is the project version 1");
+
 }
-//done with version two
+//done with version three
